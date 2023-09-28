4d32770d79696c3ac5164e473a4e6e7d82c7dccd
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-09-28 13:51:00 +0900

    fix : css style 안 먹히는 문제 해결

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,33 @@
-"use client";
+'use client'
 
-import { useState } from "react";
+import { useState } from 'react'
 
 export default function Home() {
-  const [question, setQuestion] = useState<string>("");
-  const [answer, setAnswer] = useState();
+  const [question, setQuestion] = useState('')
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    try {
-      const response = await fetch("./api/generate", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ question: question }),
-      });
-      const data = await response.json();
-      if (response.status !== 200) {
-        throw (
-          data.error ||
-          new Error(`request failed with status ${response.status}`)
-        );
-      }
-      setAnswer(data.result);
-      setQuestion("");
-    } catch (error) {
-      console.error(error);
-      alert(error);
-    }
-  };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setQuestion('')
+  }
 
   return (
-    <>
-      <form>
+    <div>
+      <h1>링클립</h1>
+      <h1 className="text-3xl font-bold underline text-cyan-500">
+        Hello world!
+      </h1>
+      <p>사용자 추가 색상</p>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           value={question}
-          onChange={(e) => setQuestion(e.target.value)}
+          onChange={(e) => {
+            setQuestion(e.target.value)
+            console.log(e.target.value)
+          }}
         />
-        <button type="submit">질문하기</button>
+        <button>질문하기</button>
       </form>
-      <div>{answer}</div>
-    </>
-  );
+    </div>
+  )
 }
