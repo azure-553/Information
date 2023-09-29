462c8d046afc167a35849a1a25abbbab360532d1
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-09-29 16:44:00 +0900

    fix : chat gpt api 연결 부분 수정

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,37 @@ import { useState } from 'react'
 
 export default function Home() {
   const [question, setQuestion] = useState('')
+  const [answer, setAnswer] = useState()
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    setQuestion('')
+    try {
+      const response = await fetch('./api/generate', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ question: question }),
+      })
+
+      const data = await response.json()
+      if (response.status !== 200) {
+        throw (
+          data.error ||
+          new Error(`request failed with status ${response.status}`)
+        )
+      }
+
+      setAnswer(data.result)
+      setQuestion('')
+    } catch (error) {
+      console.error(error)
+      // alert(error.message)
+    }
   }
 
   return (
     <div>
-      <h1 className="text-my-color">링클립</h1>
-      <h1 className="text-3xl font-bold underline text-cyan-500">
-        Hello world!
-      </h1>
-      <p>사용자 추가 색상</p>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
@@ -25,9 +43,11 @@ export default function Home() {
             setQuestion(e.target.value)
             console.log(e.target.value)
           }}
+          className="rounded-lg border-transparent flex-1 appearance-none border border-gray-900 w-[800px] py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
         />
-        <button>질문하기</button>
+        <button className="bg-slate-500">질문하기</button>
       </form>
+      <div>{answer}</div>
     </div>
   )
 }
